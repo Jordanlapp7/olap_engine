dde7d3a380ecae29088d90af36f89962809e7302
Jordan Lapp <jordanlapp7@gmail.com>
2025-05-02 20:09:56 -0400

    Implemented csv reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod table;
+mod plan;
+mod execution;
+mod types;
+mod util;
+
 fn main() {
-    println!("Hello, world!");
+    println!("OLAP Engine Initialized.");
+    // TODO: accept CLI args to load CSV and run queries
 }
--- src/table.rs
+++ src/table.rs
@@ -1,4 +1,7 @@
 use std::collections::HashMap;
+use std::fs::File;
+use std::error::Error;
+use csv;
 
 pub struct Column<T> {
   pub name: String,
@@ -10,10 +13,27 @@ pub struct Table {
 }
 
 impl Table {
-  pub fn load_csv(_path: &str) -> Self {
-    // TODO: implement CSV parsing
-    Table {
-      columns: HashMap::new()
+  pub fn load_csv(path: &str) -> Result<Self, Box<dyn Error>> {
+    let mut reader = csv::Reader::from_path(path)?;
+    let headers = reader.headers()?.clone();
+
+    let mut columns: HashMap<String, Column<String>> = HashMap::new();
+
+    for header in headers.iter() {
+      columns.insert(header.to_string(), Column {
+        name: header.to_string(),
+        data: Vec::new(),
+      });
     }
+
+    for record in reader.records() {
+      let record = record?;
+      for  (i, field) in record.iter().enumerate() {
+        let header = &headers[i];
+        columns.get_mut(header).unwrap().data.push(field.to_string());
+      }
+    }
+
+    Ok(Table { columns })
   }
 }
